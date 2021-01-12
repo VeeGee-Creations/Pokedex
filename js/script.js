cfd347df5522eec2093e937fa17a84497c166444
Vincent Geesman <vgeesman@gmail.com>
2021-01-12 01:42:16 -0800

    fixed quotes

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ let favFood = '';
 
 
 function fed() {
-    if (favFood === "Nothing" || favFood === '') {
-        document.getElementById("response").innerHTML = "<br/>Bunny is sad.</br>Will you please feed Bunny?";
+    if (favFood === 'Nothing' || favFood === '') {
+        document.getElementById('response').innerHTML = '<br/>Bunny is sad.</br>Will you please feed Bunny?';
     }
-    else if (favFood === "Carrot Cake") {
-        document.getElementById('response').innerHTML = "<br/>Wow! You brought Bunny's Favorite Snack!<br/> Bunny Is beaming with joy!"
+    else if (favFood === 'Carrot Cake') {
+        document.getElementById('response').innerHTML = '<br/>Wow! You brought Bunny\'s Favorite Snack!<br/> Bunny Is beaming with joy!'
     }
     else  {
-        document.getElementById("response").innerHTML = "<br/>Bunny ate the " + favFood + ". He loves it!<br/>Try giving him another snack!";
+        document.getElementById('response').innerHTML = '<br/>Bunny ate the ' + favFood + '. He loves it!<br/>Try giving him another snack!';
     }
 }
 
 function feed() {
-    favFood = document.getElementById("snack").value;
+    favFood = document.getElementById('snack').value;
     fed();
 }
