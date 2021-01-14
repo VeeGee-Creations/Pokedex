8e06985c32329dfce2dfc07d96cb641c8f9efa38
Vincent Geesman <vgeesman@gmail.com>
2021-01-14 09:27:24 -0800

    created pokelist{}, added details of pokemon 1-9

--- js/script.js
+++ js/script.js
@@ -1,21 +1,169 @@
-alert('Hello World');
+let pokemonList = {};
 
-let favFood = '';
-
-
-function fed() {
-    if (favFood === 'Nothing' || favFood === 'nothing' || favFood === '') {
-        document.getElementById('response').innerHTML = '<br/>Bunny is sad.</br>Will you please feed Bunny?';
+pokemonList.p001 = {
+    name: 'Bulbasaur',
+    category: 'Seed',
+    height: 2.04,
+    weight: 15.2,
+    abilities: {
+        abbility1: 'Overgrow'
+    },
+    type: {
+        type1: 'Grass',
+        type2: 'Poison'
+    },
+    weakness: {
+        weakness1: 'Fire',
+        weakness2: 'Psychic',
+        weakness3: 'Flying',
+        weakness4: 'Ice'
     }
-    else if (favFood === 'Carrot Cake') {
-        document.getElementById('response').innerHTML = '<br/>Wow! You brought Bunny\'s Favorite Snack!<br/> Bunny Is beaming with joy!'
-    }
-    else  {
-        document.getElementById('response').innerHTML = '<br/>Bunny ate the ' + favFood + '. He loves it!<br/>Try giving him another snack!';
-    }
-}
+};
 
-function feed() {
-    favFood = document.getElementById('snack').value;
-    fed();
-}
+pokemonList.p002 = {
+    name: 'Ivysaur',
+    category: 'Seed',
+    height: 3.03,
+    weight: 28.7,
+    abilities: {
+        abbility1: 'Overgrow'
+    },
+    type: {
+        type1: 'Grass',
+        type2: 'Poison'
+    },
+    weakness: {
+        weakness1: 'Fire',
+        weakness2: 'Psychic',
+        weakness3: 'Flying',
+        weakness4: 'Ice'
+    }
+};
+
+pokemonList.p003 = {
+    name: 'Venusaur',
+    category: 'Seed',
+    height: 6.07,
+    weight: 220.5,
+    abilities: {
+        abbility1: 'Overgrow'
+    },
+    type: {
+        type1: 'Grass',
+        type2: 'Poison'
+    },
+    weakness: {
+        weakness1: 'Fire',
+        weakness2: 'Psychic',
+        weakness3: 'Flying',
+        weakness4: 'Ice'
+    }
+};
+
+pokemonList.p004 = {
+    name: 'Charmander',
+    category: 'Lizard',
+    height: 2.00,
+    weight: 18.7,
+    abilities: {
+        abbility1: 'Blaze'
+    },
+    type: {
+        type1: 'Fire'
+    },
+    weakness: {
+        weakness1: 'Water',
+        weakness2: 'Ground',
+        weakness3: 'Rock'
+    }
+};
+
+pokemonList.p005 = {
+    name: 'Charmeleon',
+    category: 'Flame',
+    height: 3.07,
+    weight: 41.9,
+    abilities: {
+        abbility1: 'Blaze'
+    },
+    type: {
+        type1: 'Fire'
+    },
+    weakness: {
+        weakness1: 'Water',
+        weakness2: 'Ground',
+        weakness3: 'Rock'
+    }
+};
+
+pokemonList.p006 = {
+    name: 'Charizard',
+    category: 'Flame',
+    height: 5.07,
+    weight: 199.5,
+    abilities: {
+        abbility1: 'Blaze'
+    },
+    type: {
+        type1: 'Fire',
+        type2: 'Flying'
+    },
+    weakness: {
+        weakness1: 'Water',
+        weakness2: 'Electric',
+        weakness3: 'Rock'
+    }
+};
+
+pokemonList.p007 = {
+    name: 'Squirtle',
+    category: 'Tiny Turtle',
+    height: 1.08,
+    weight: 19.8,
+    abilities: {
+        abbility1: 'Torrent'
+    },
+    type: {
+        type1: 'Water'
+    },
+    weakness: {
+        weakness1: 'Grass',
+        weakness2: 'Electric'
+    }
+};
+
+pokemonList.p008 = {
+    name: 'Wartortle',
+    category: 'Turtle',
+    height: 3.03,
+    weight: 49.6,
+    abilities: {
+        abbility1: 'Torrent'
+    },
+    type: {
+        type1: 'Water'
+    },
+    weakness: {
+        weakness1: 'Grass',
+        weakness2: 'Electric'
+    }
+};
+
+pokemonList.p009 = {
+    name: 'Blastoise',
+    category: 'Shellfish',
+    height: 5.03,
+    weight: 188.5,
+    abilities: {
+        abbility1: 'Torrent'
+    },
+    type: {
+        type1: 'Water'
+    },
+    weakness: {
+        weakness1: 'Grass',
+        weakness2: 'Electric'
+    }
+};
+
+console.log(pokemonList);
